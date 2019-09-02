218b2cf8f48582446f9a664229efa1a1a8a29200
Mark Skelton <mdskelton99@gmail.com>
2019-09-02 10:36:17 -0500

    Add check for updates function

--- src/auto-updates.ts
+++ src/auto-updates.ts
@@ -1,28 +1,25 @@
-import { app, Notification } from 'electron'
+import { app } from 'electron'
 import log from 'electron-log'
 import { autoUpdater } from 'electron-updater'
 import { is } from 'electron-util'
 
+import { viewLogs } from './logs'
+import { createNotification } from './notifications'
+
 const UPDATE_CHECK_INTERVAL = 60000 * 60 * 3 // 3 Hours
 
 function onUpdateAvailable(): void {
-  const notification = new Notification({
-    actions: [
-      {
-        type: 'button',
-        text: 'Restart'
-      }
-    ],
-    body: 'Please restart to update to the latest version',
-    title: 'Update available'
-  })
-
-  notification.on('action', () => {
-    app.relaunch()
-    app.quit()
-  })
-
-  notification.show()
+  createNotification(
+    'Update available',
+    'Please restart to update to the latest version',
+    {
+      action: () => {
+        app.relaunch()
+        app.quit()
+      },
+      text: 'Restart'
+    }
+  )
 }
 
 export function init(): void {
@@ -34,5 +31,22 @@ export function init(): void {
 
     setInterval(() => autoUpdater.checkForUpdates, UPDATE_CHECK_INTERVAL)
     autoUpdater.checkForUpdates()
+  }
+}
+
+export async function checkForUpdates(): Promise<void> {
+  try {
+    await autoUpdater.checkForUpdates()
+  } catch (error) {
+    log.error('Check for updates failed', error)
+
+    createNotification(
+      'Check for updates failed',
+      'View the logs for more information',
+      {
+        action: viewLogs,
+        text: 'View Logs'
+      }
+    )
   }
 }
