3c2b4e0e9427f0f13f6aa631c591fc9af3a25f3a
zzpxyx <zptomz@gmail.com>
2019-10-14 00:12:50 -0500

    Fix null reference issue on non-Linux.

--- src/app.ts
+++ src/app.ts
@@ -186,30 +186,25 @@ app.on('ready', () => {
 
     const contextMenuTemplate: MenuItemConstructorOptions[] = [
       {
+        click: () => {
+          mainWindow.show()
+        },
+        label: 'Show',
+        visible: shouldStartMinimized,
+        id: 'show-win'
+      },
+      {
+        label: 'Hide',
+        visible: !shouldStartMinimized,
+        click: () => {
+          mainWindow.hide()
+        },
+        id: 'hide-win'
+      },
+      {
         role: 'quit'
       }
     ]
-
-    if (is.linux) {
-      contextMenuTemplate.unshift(
-        {
-          click: () => {
-            mainWindow.show()
-          },
-          label: 'Show',
-          visible: shouldStartMinimized,
-          id: 'show-win'
-        },
-        {
-          label: 'Hide',
-          visible: !shouldStartMinimized,
-          click: () => {
-            mainWindow.hide()
-          },
-          id: 'hide-win'
-        }
-      )
-    }
 
     trayContextMenu = Menu.buildFromTemplate(contextMenuTemplate)
 
