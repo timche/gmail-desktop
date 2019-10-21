9e32d80278865b1ba0b8ef4ad1a9aad099d7c8bf
Tim Cheung <tim@cheung.io>
2019-10-21 10:09:58 +0100

    fix getting account id from url
    fixes #170

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,7 +2,7 @@ import { platform as selectPlatform } from 'electron-util'
 
 // URL: `mail.google.com/mail/u/<local_account_id>`
 export function getUrlAccountId(url: string): null | string {
-  const accountIdRegExp = /mail\/u\/(\d+)\//
+  const accountIdRegExp = /mail\/u\/(\d+)/
   const res = accountIdRegExp.exec(url)
   return res && res[1]
 }
