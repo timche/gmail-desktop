9629fefcd4cefe84138eddf04331ff29b5ab88d5
Tim Cheung <tim@cheung.io>
2021-03-19 02:19:22 +0000

    meta tweaks

--- src-main/constants.ts
+++ src-main/constants.ts
@@ -1,10 +1,5 @@
-import { app } from 'electron'
-import config, { ConfigKey } from './config'
-
-export const SHOULD_START_MINIMIZED =
-  app.commandLine.hasSwitch('launch-minimized') ||
-  config.get(ConfigKey.LaunchMinimized)
-
 export const ACCOUNTS_TAB_HEIGHT = 40
 
 export const GMAIL_URL = 'https://mail.google.com'
+
+export const DEFAULT_ACCOUNT_ID = 'default'
