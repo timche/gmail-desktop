7615a93bd7eb6002919b187a615c5b63155b2e0f
Tim Cheung <tim@cheung.io>
2021-03-20 17:09:52 +0000

    bundle main

--- scripts/compile-renderer.js
+++ scripts/compile-renderer.js
@@ -1,46 +1,10 @@
-const esbuild = require('esbuild')
+const compile = require('./utils/compile')
 
-const isProd = process.env.NODE_ENV === 'production'
-
-function logBuildSuccessful() {
-  console.log('Build succeeded')
-}
-
-function logBuildFailure(error) {
-  console.error('Build failed:', error)
-}
-
-esbuild
-  .build({
-    entryPoints: ['./src-renderer/index.tsx'],
-    bundle: true,
-    target: 'chrome89',
-    sourcemap: !isProd,
-    outfile: './dist-renderer/index.js',
-    define: {
-      'process.env.NODE_ENV': isProd ? '"production"' : '"development"'
-    },
-    watch: isProd
-      ? undefined
-      : {
-          onRebuild: (error) => {
-            if (error) {
-              logBuildFailure(error)
-              return
-            }
-
-            logBuildSuccessful()
-          }
-        }
-  })
-  .then(({ stop }) => {
-    logBuildSuccessful()
-
-    if (isProd && stop) {
-      stop()
-    }
-  })
-  .catch((error) => {
-    logBuildFailure(error)
-    throw error
-  })
+compile('renderer', (isProd) => ({
+  entryPoints: ['./src-renderer/index.tsx'],
+  target: 'chrome89',
+  outfile: './dist-renderer/index.js',
+  define: {
+    'process.env.NODE_ENV': isProd ? '"production"' : '"development"'
+  }
+}))
