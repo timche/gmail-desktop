ed2554fbc7620fc68531d09d1654fb3aa1482cfc
Tim Cheung <tim@cheung.io>
2021-04-08 00:11:00 +0100

    show url preview when hovering over external links

---
+++ src/main/account-views/preload/url-preview.ts
@@ -0,0 +1,26 @@
+import { darkTheme } from '../../../theme'
+import { gmailUrl } from '../../../constants'
+
+let urlPreviewElement: HTMLDivElement | null = null
+
+export function initUrlPreview() {
+  window.addEventListener('mouseover', (event) => {
+    const target = event.target as HTMLAnchorElement | HTMLElement
+
+    if ('href' in target && !target.href.startsWith(gmailUrl)) {
+      urlPreviewElement = document.createElement('div')
+      urlPreviewElement.className = 'urlPreview'
+      urlPreviewElement.style.color = darkTheme.text.mediumEmphasis
+      urlPreviewElement.style.background = darkTheme.bg[2]
+      urlPreviewElement.textContent = target.href
+      document.body.append(urlPreviewElement)
+    }
+  })
+
+  window.addEventListener('mouseout', () => {
+    if (urlPreviewElement) {
+      urlPreviewElement.remove()
+      urlPreviewElement = null
+    }
+  })
+}
