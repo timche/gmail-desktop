b5b937dd1f40212478062e86861162d32c8185ac
Tim Cheung <tim@cheung.io>
2021-04-09 16:38:50 +0100

    add only needed icons as react components

---
+++ src/renderer/TitleBar/icons/Ellipsis.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function EllipsisIcon() {
+  return (
+    <svg
+      width="16"
+      height="16"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+    >
+      <path d="M4 8a1 1 0 11-2 0 1 1 0 012 0zm5 0a1 1 0 11-2 0 1 1 0 012 0zm5 0a1 1 0 11-2 0 1 1 0 012 0z" />
+    </svg>
+  )
+}
---
+++ src/renderer/TitleBar/icons/Maximize.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function MaximizeIcon() {
+  return (
+    <svg
+      width="16"
+      height="16"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+    >
+      <path d="M3 3v10h10V3H3zm9 9H4V4h8v8z" />
+    </svg>
+  )
+}
